042871a3e333eeda372e40bd390d83a5b0d057d0
subraban <136383571+subraban@users.noreply.github.com>
2023-07-08 15:43:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_sql_database_instance" "instance" {
-  name             = "sqlserver-instance"
+  name             = "instance1"
   region           = "us-central1"
   database_version = "SQLSERVER_2017_EXPRESS"
   root_password    = "Prakash@123"
@@ -12,6 +12,11 @@ resource "google_sql_database_instance" "instance" {
 }
 
 resource "google_sql_database" "database" {
-  name     = "my-database"
+  name     = "db1"
   instance = google_sql_database_instance.instance.name
+}
+resource "null_resource" "import_backup" {
+  provisioner "local-exec" {
+    command = "gcloud sql import sql instance1 gs://sqlservermedia/WideWorldImporters-Full.bak.bak --database=db1 --user=sqlserver --password=Prakash@123 --quiet"
+  }
 }
